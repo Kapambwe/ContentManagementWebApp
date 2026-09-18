ebf3a08a3f1b003f23a6152e7f6e38cc557a2add
Local Azure DevOps Emulator <local-azure-devops@localhost>
2026-09-18 18:32:26 +0200

    deploy: ContentManagementWebApp 2026-09-18 18:32:26

--- _framework/dotnet.grmevchbed.js
+++ _framework/dotnet.grmevchbed.js
@@ -1304,8 +1304,8 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "debugLevel": 0,
   "appsettings": [
     "../appsettings.Development.json",
-    "../appsettings.json",
-    "../appsettings.github.json"
+    "../appsettings.github.json",
+    "../appsettings.json"
   ],
   "globalizationMode": "sharded",
   "extensions": {
